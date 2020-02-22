ed7a641a4ee544065f260a5b5df4248c94f917c3
Srimalaya <ladha@sfu.ca>
2020-02-21 16:24:11 -0800

    SharedPreferences working
    Additionally, added sound effects

--- app/src/main/java/com/example/cmpt276_a3/ca/cmpt276A3/view/OptionsActivity.java
+++ app/src/main/java/com/example/cmpt276_a3/ca/cmpt276A3/view/OptionsActivity.java
@@ -2,6 +2,7 @@ package com.example.cmpt276_a3.ca.cmpt276A3.view;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.example.cmpt276_a3.ca.cmpt276A3.model.MineManager;
@@ -15,6 +16,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextClock;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.cmpt276_a3.R;
@@ -38,15 +41,24 @@ public class OptionsActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar_help);
         setSupportActionBar(toolbar);
 
-        createRadioNumMines();
-        createRadioSizes();
         manager = MineManager.getInstance();
+
+        int savedCount = getNumMinesSelected(this);
+        manager.setCount(savedCount);
+
+        int savedRows = getNumRowsSelected(this);
+        int savedCols = getNumColsSelected(this);
+        manager.setColumns(savedCols);
+        manager.setRows(savedRows);
+
         manager.populateMines();
 
+        createRadioNumMines();
+        createRadioSizes();
         checkTotalReset();
-
         hideUIelements();
     }
+
 
     /**
      * Citation: Learned from developer.android.com/reference/android/view
@@ -92,12 +104,19 @@ public class OptionsActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     manager.setCount(mineOption);
+                    saveCount(mineOption);
+
                     manager.populateMines();
                 }
             });
 
             // Add to radio group
             numMines.addView(button);
+
+            // Select default button:
+            if(mineOption == getNumMinesSelected(this)) {
+                button.setChecked(true);
+            }
         }
     }
 
@@ -122,22 +141,79 @@ public class OptionsActivity extends AppCompatActivity {
                 public void onClick(View v) {
                     manager.setRows(rowOption);
                     manager.setColumns(colOption);
+                    saveCols(colOption);
+                    saveRows(rowOption);
+
                     manager.populateMines();
                 }
             });
 
             // Add to radio group
             sizes.addView(button);
+
+            // Select default button:
+            if(rowOption == getNumRowsSelected(this)) {
+                button.setChecked(true);
+            }
         }
     }
 
-    private void checkTotalReset() {
+    private void saveTotal(int total) {
+        SharedPreferences pref = this.getSharedPreferences("AppPreferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putInt("Total times played", total);
+        editor.apply();
+    }
 
+    private void saveCount(int mineOption) {
+        SharedPreferences pref = this.getSharedPreferences("AppPreferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putInt("Num of mines selected", mineOption);
+        editor.apply();
+    }
+
+    private void saveRows(int rowOption) {
+        SharedPreferences pref = this.getSharedPreferences("AppPreferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putInt("Num of rows selected", rowOption);
+        editor.apply();
+    }
+
+    private void saveCols(int colOption) {
+        SharedPreferences pref = this.getSharedPreferences("AppPreferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putInt("Num of columns selected", colOption);
+        editor.apply();
+    }
+
+    static public int getNumMinesSelected(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("AppPreferences", MODE_PRIVATE);
+
+        int defaultValue = context.getResources().getInteger(R.integer.default_num_mines);
+        return prefs.getInt("Num of mines selected", defaultValue);
+    }
+
+    static public int getNumRowsSelected(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("AppPreferences", MODE_PRIVATE);
+
+        int defaultValue = context.getResources().getInteger(R.integer.default_num_rows);
+        return prefs.getInt("Num of rows selected", defaultValue);
+    }
+
+    static public int getNumColsSelected(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("AppPreferences", MODE_PRIVATE);
+
+        int defaultValue = context.getResources().getInteger(R.integer.default_num_cols);
+        return prefs.getInt("Num of columns selected", defaultValue);
+    }
+
+    private void checkTotalReset() {
         Button button = findViewById(R.id.btnReset);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(OptionsActivity.this, "Reset Total", Toast.LENGTH_SHORT).show();
+                saveTotal(0);
             }
         });
     }
